365bbd3a729d43b10605f01e3ffcd99a2b007ac1
tuskenincursor@gmail.com <tuskenincursor@gmail.com>
2023-04-11 23:55:40 -0300

    primer commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TP1.Models;
+using TP1.Services;
+
+namespace TP1.Pages;
+
+public class IndexModel : PageModel
+{
+    public List<Movie> MovieList{get; set;}
+    public Movie NewMovie { get; set; }
+    
+    public IndexModel()
+    {
+    }
+
+    public void OnGet()
+    {
+      MovieList = MovieService.GetAll();
+    }
+    
+    public IActionResult OnPost(){
+        if(!ModelState.IsValid){
+            return RedirectToPage("/Error");
+        }
+        
+        MovieService.Add(NewMovie);
+
+        return RedirectToAction("get");
+    }
+}
---
+++ Services/MovieService.cs
@@ -0,0 +1,36 @@
+using TP1.Models;
+
+namespace TP1.Services;
+
+public static class MovieService{
+    static List<Movie> Movies { get; set;}
+
+    static MovieService(){
+        Movies = new List<Movie>
+        {
+            new Movie { Name = "Star Wars", Code="STW", Category="Sci fi", Minutes=110, Review="⭐⭐⭐⭐"},
+            new Movie { Name = "Avatar", Code="AVT", Category="Sci fi", Minutes=500, Review="⭐⭐"},
+            new Movie { Name = "Hannibal", Code="HNL", Category="Terror", Minutes=110, Review="⭐⭐"}
+        };
+    }
+
+      public static List<Movie> GetAll() => Movies;
+
+       public static void Add(Movie obj){
+       if(obj == null){
+          return;
+        }
+
+      Movies.Add(obj);
+    }
+
+//     public static void Delete(string code){
+//         var movieToDelete = Get(code);
+
+//         if (movieToDelete != null){
+//             Movies.Remove(movieToDelete);
+//         }
+//     }
+//     public static Movie? Get(string code) => Movies.FirstOrDefault(x => x.Code.ToLower() == code.ToLower());
+// }
+}
